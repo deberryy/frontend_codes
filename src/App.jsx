e5175102435b5b0245e105e18a2e78f7a7f382ba
Patrick-123-FTY <pessuman020@st.ug.edu.gh>
2025-09-12 17:41:02 +0000

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,7 @@ const App = () => {
   const [isModalOpen, setIsModalOpen] = useState(false); const [currentPayment, setCurrentPayment] = useState(null);
   const [isAuthLoading, setIsAuthLoading] = useState(false);
 
-  const API_URL = "https://semester-project-k7nj.onrender.com" //'http://localhost:5000/api'; // Backend API URL
-
+  const API_URL = "https://semester-project-k7nj.onrender.com" 
   // Custom function to handle API calls with authorization
   const apiCall = async (endpoint, method, body = null) => {
     const headers = { 'Content-Type': 'application/json' };
